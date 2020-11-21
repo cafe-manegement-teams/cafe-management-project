78d21ffe1f314ece90c17b72f2286b4574e97f49
Nguyen Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2020-11-21 23:29:51 +0700

    handle function goods

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Warehouses from "./components/WareHouses/WareHouses";
 import Receipt from "./components/Receipt/Receipt";
 import Delivery from "./components/Delivery/Delivery";
 import Supplier from "./components/Supplier/Supplier";
+import CreateGoods from "./components/Goods/CreateGoods";
 
 import "./App.css";
 
@@ -22,70 +23,67 @@ function App() {
           <Home />
         </div>
       </Route>
-
       <Route path="/staff">
         <div className="App">
           <Sidebar />
           <Staff />
         </div>
       </Route>
-
-      <Route path="/goods">
+      // goods
+      <Route path="/goods" exact>
         <div className="App">
           <Sidebar />
           <Goods />
         </div>
       </Route>
-      
+      <Route path="/goods/create">
+        <div className="App">
+          <Sidebar />
+          <CreateGoods />
+        </div>
+      </Route>
       <Route path="/warehouses">
         <div className="App">
           <Sidebar />
           <Warehouses />
         </div>
       </Route>
-
       <Route path="/receiptbill">
         <div className="App">
           <Sidebar />
           <Receipt />
         </div>
       </Route>
-
       <Route path="/deliverybill">
         <div className="App">
           <Sidebar />
           <Delivery />
         </div>
       </Route>
-
       <Route path="/supplier">
         <div className="App">
           <Sidebar />
           <Supplier />
         </div>
       </Route>
-
       <Route path="/report/CostReport">
         <div className="App">
           <Sidebar />
           <CostReport />
         </div>
       </Route>
-
       <Route path="/report/RevenueReport">
         <div className="App">
           <Sidebar />
           <RevenueReport />
         </div>
       </Route>
-
       <Route path="/report/SaleReport">
         <div className="App">
           <Sidebar />
           <SaleReport />
         </div>
       </Route>
-      
     </Switch>
   );
 }
--- src/components/Goods/Goods.js
+++ src/components/Goods/Goods.js
@@ -1,7 +1,58 @@
-import React from "react";
+import * as React from "react";
+import {Link} from "react-router-dom";
+import "./Goods.css";
+
+import { DataGrid } from "@material-ui/data-grid";
+
+import SearchIcon from "@material-ui/icons/Search"; 
+import AddIcon from "@material-ui/icons/Add";
+
+const columns = [
+  { field: "id", headerName: "Mã Hàng Hóa", width: 150 },
+  { field: "nameGood", headerName: "Tên Hàng Hóa", width: 230 },
+  { field: "unit", headerName: "Đơn Vị Tính", width: 230 },
+  { field: "price", headerName: "Giá", width: 230 },
+];
+
+const rows = [
+  { id: 1, nameGood: "Cafe", unit: "bịch", price: 550.0 },
+  { id: 2, nameGood: "Mango tea", unit: "thùng", price: 400.0 },
+  { id: 3, nameGood: "Peach tea", unit: "thùng", price: 450.0 },
+  { id: 4, nameGood: "Lychee tea", unit: "thùng", price: 500.0 },
+  { id: 5, nameGood: "Socola", unit: "thùng", price: 800.0 },
+  { id: 6, nameGood: "Caramel", unit: "thùng", price: 640.0 },
+  { id: 7, nameGood: "Apple tea", unit: "thùng", price: 350.0 },
+  { id: 8, nameGood: "Green tea", unit: "thùng", price: 600.0 },
+  { id: 9, nameGood: "Jasmine tea", unit: "thùng", price: 650.0 },
+];
 
 function Goods() {
-  return <div>this is goods</div>;
+  return (
+    <div className="goods">
+      <div className="header">
+        <div className="header__left">
+          <h1>Hàng Hóa</h1>
+        </div>
+        <div className="header__middle">
+          <SearchIcon className="header__middle--icon" />
+          <input placeholder="Search here" className="header__middle--input" />
+        </div>
+        <div className="header__right">
+          <AddIcon className="header__right--icon" />
+          <Link to="goods/create"><p>Thêm Hàng Hóa</p></Link>
+        </div>
+      </div>
+      {/* content table */}
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          checkboxSelection
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Goods;
