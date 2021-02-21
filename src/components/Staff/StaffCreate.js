30027c1c877098e0df0d9574dd15fe3ea391895e
Nguyen Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2021-02-21 12:38:56 +0700

    edit code form nhan vien

--- src/components/Staff/StaffCreate.js
+++ src/components/Staff/StaffCreate.js
@@ -4,23 +4,23 @@ import "./StaffCreate.css";
 function StaffCreate() {
   return (
     <div className="container">
-      <div class="row header">
-        <h1>NHAN VIEN &nbsp;</h1>
+      <div className="row header">
+        <h1>NHÂN VIÊN &nbsp;</h1>
         <h3>Fill out the form below to learn more!</h3>
       </div>
       <form action="#">
         <ul>
           <li>
             <p>
-              <label for="first_name">Ma Nhan Vien</label>
+              <label htmlFor="first_name">Ma Nhan Vien</label>
               <input type="text" name="manhanvien" placeholder="001" />
             </p>
             <p>
-              <label for="last_name">Ten Nhan Vien</label>
+              <label htmlFor="last_name">Ten Nhan Vien</label>
               <input type="text" name="tennhanvien" placeholder="Smith" />
             </p>
             <p>
-              <label for="last_name">Dia Chi</label>
+              <label htmlFor="last_name">Dia Chi</label>
               <input
                 type="text"
                 name="diachi"
@@ -28,20 +28,20 @@ function StaffCreate() {
               />
             </p>
             <p>
-              <label for="last_name">Salary</label>
+              <label htmlFor="last_name">Salary</label>
               <input type="text" name="salary" placeholder="3000" />
             </p>
           </li>
           <li>
             <p>
-              <label for="phone">
-                phone <span class="req">*</span>
+              <label htmlFor="phone">
+                phone <span className="req">*</span>
               </label>
               <input type="text" name="phone" placeholder="0913234566" />
             </p>
           </li>
           <li>
-            <input class="btn btn-submit" type="submit" value="Submit" />
+            <input className="btn btn-submit" type="submit" value="Submit" />
             <small>
               or press <strong>enter</strong>
             </small>
